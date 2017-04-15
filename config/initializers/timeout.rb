37a862a16274f3d388e7dd1c286a30fe31cea82a
alpaca-tc <alpaca-tc@alpaca.tc>
2017-04-15 22:05:06 +0900

    remote_followするときに90秒待たないようにする

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -4,3 +4,22 @@ Rack::Timeout.service_timeout = false
 if Rails.env.production?
   Rack::Timeout.service_timeout = 30
 end
+
+require 'goldfinger'
+
+module Goldfinger
+  class Request
+    module ShortTimeout
+      private
+
+      def http_client
+        # original
+        # HTTP.timeout(:per_operation, write: 60, connect: 20, read: 60).follow
+
+        HTTP.timeout(:per_operation, write: 2, connect: 2, read: 2).follow
+      end
+    end
+
+    prepend(ShortTimeout)
+  end
+end
