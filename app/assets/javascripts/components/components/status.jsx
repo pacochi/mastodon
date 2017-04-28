1d338bc6d6f80c90c76b90d14f5bbb0af826fa90
alpaca-tc <alpaca-tc@alpaca.tc>
2017-04-28 22:02:54 +0900

    Revert "card in map"
    This reverts commit e7800b8702cb13765ea74bdd6510d36d35139649.

--- app/assets/javascripts/components/components/status.jsx
+++ app/assets/javascripts/components/components/status.jsx
@@ -83,17 +83,15 @@ const Status = React.createClass({
 
     let attachments = status.get('media_attachments');
     if (status.getIn(['pixiv_cards'], Immutable.List()).size > 0) {
-      attachments = status.get('pixiv_cards').map(card => {
-        const image_url = card.get('image_url').replace(/http:/, 'https:');
-        return Immutable.fromJS({
-          id: Math.random().toString(),
-          preview_url: image_url,
-          remote_url: '',
-          text_url: card.get('url'),
-          type: 'image',
-          url: image_url
-        });
-      });
+      const image_url = card.get('image_url').replace(/http:/, 'https:');
+      attachments = status.get('pixiv_cards').map(card => Immutable.fromJS({
+        id: Math.random().toString(),
+        preview_url: image_url,
+        remote_url: '',
+        text_url: card.get('url'),
+        type: 'image',
+        url: image_url
+      }));
     }
 
     if (attachments.size > 0 && !this.props.muted) {
