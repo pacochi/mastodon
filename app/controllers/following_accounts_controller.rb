43740e253f3824d9c251eb466c05a294023a9934
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-06 11:03:07 +0900

    Optimize follower_accounts and following_accounts (#2820)

--- app/controllers/following_accounts_controller.rb
+++ app/controllers/following_accounts_controller.rb
@@ -6,7 +6,7 @@ class FollowingAccountsController < ApplicationController
   BOT_ACCOUNT_IDS = [134316, 109687, 6655, 135511, 151546, 8595, 320, 112251, 135602, 47592, 50776, 128145, 134831, 135026, 16896, 109545, 131540, 189622, 189263, 4273, 73311, 65541, 200022, 134823, 163458, 145150, 51908, 20205, 86842, 7647, 168346, 134617, 51010, 2455, 131396, 134553].freeze
 
   def index
-    @accounts = @account.following.page(params[:page]).per(FOLLOW_PER_PAGE)
+    @follows = Follow.where(account: @account).order(id: :desc).page(params[:page]).per(FOLLOW_PER_PAGE).preload(:target_account)
   end
 
   private
