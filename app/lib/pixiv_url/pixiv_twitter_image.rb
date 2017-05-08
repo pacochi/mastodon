a7d0c14dc563986e2c6ac9d01cd2b2ae24b95ca7
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-08 12:16:02 +0900

    Use common http helper

--- app/lib/pixiv_url/pixiv_twitter_image.rb
+++ app/lib/pixiv_url/pixiv_twitter_image.rb
@@ -3,6 +3,8 @@ module PixivUrl
     EXPIRES_IN = 12.hours
 
     class << self
+      include HttpHelper
+
       def cache_or_fetch(url, force: false)
         return unless PixivUrl.valid_pixiv_url?(url)
 
@@ -22,18 +24,12 @@ module PixivUrl
       end
 
       def fetch_image_url(url)
-        response = client.get(url, headers: { 'Referer' => "https://#{Rails.configuration.x.local_domain}" })
+        response = http_client.get(url, headers: { 'Referer' => "https://#{Rails.configuration.x.local_domain}" })
         return unless response.status == 200
 
         html = Nokogiri::HTML.parse(response.body.to_s)
         url = html.xpath('/html/head/meta[@property="twitter:image"]/@content').to_s
         url if PixivUrl.valid_twitter_image?(url)
-      end
-
-      def client
-        HTTP.headers(
-          user_agent: FetchLinkCardService::USER_AGENT
-        ).timeout(:per_operation, write: 10, connect: 10, read: 10).follow
       end
     end
   end
