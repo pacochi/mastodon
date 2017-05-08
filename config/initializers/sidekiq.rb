70ecf3b4af7b521d67b09649a15c860ff223042e
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-08 12:00:17 +0900

    Optimize sidekiq_scheduler
    - add routes for sidekiq scheduler
    - extract scheduler to config/sidekiq_scheduler.yml because systemd doesn't use configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
+require 'sidekiq'
+require 'sidekiq/scheduler'
+
 host = ENV.fetch('REDIS_HOST') { 'localhost' }
 port = ENV.fetch('REDIS_PORT') { 6379 }
 password = ENV.fetch('REDIS_PASSWORD') { false }
@@ -5,6 +8,11 @@ db = ENV.fetch('REDIS_DB') { 0 }
 
 Sidekiq.configure_server do |config|
   config.redis = { host: host, port: port, db: db, password: password }
+
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_scheduler.yml', __FILE__))
+    Sidekiq::Scheduler.reload_schedule!
+  end
 end
 
 Sidekiq.configure_client do |config|
