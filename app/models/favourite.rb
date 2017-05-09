12138cee4ac16121157e2d25142e463f12a8cefa
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-09 19:58:02 +0900

    Delete no_deadlock_callbacks
    多分色々最適化したので、デッドロックならなさそう。

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,11 +2,9 @@
 
 class Favourite < ApplicationRecord
   include Paginable
-  include NoDeadlockCallbacks
 
   belongs_to :account, inverse_of: :favourites, required: true
-  belongs_to :status,  inverse_of: :favourites, required: true
-  no_deadlock_callback(:status, counter_cache: true)
+  belongs_to :status,  inverse_of: :favourites, required: true, counter_cache: true
 
   has_one :notification, as: :activity, dependent: :destroy
 
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,15 +2,13 @@
 
 class Follow < ApplicationRecord
   include Paginable
-  include NoDeadlockCallbacks
 
-  belongs_to :account, required: true
-  no_deadlock_callback(:account, counter_cache: 'following_count')
+  belongs_to :account, required: true, counter_cache: 'following_count'
 
   belongs_to :target_account,
              class_name: 'Account',
-             required: true
-  no_deadlock_callback(:target_account, counter_cache: 'followers_count')
+             required: true,
+             counter_cache: 'followers_count'
 
   has_one :notification, as: :activity, dependent: :destroy
 
