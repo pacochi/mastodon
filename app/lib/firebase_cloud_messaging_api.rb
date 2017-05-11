a9870cb9151e94bba6eaf3976cf28a6d46502cf1
yuutetu <yuutetu@pixiv.com>
2017-05-11 18:39:38 +0900

    iOS用のFCMのjsonを調整

--- app/lib/firebase_cloud_messaging_api.rb
+++ app/lib/firebase_cloud_messaging_api.rb
@@ -4,7 +4,13 @@ class FirebaseCloudMessagingApi
   FCM_ENDPOINT = 'https://fcm.googleapis.com'
 
   class << self
-    def publish(to, data)
+    def publish(to, data, platform)
+      if platform == 'iOS'
+        publish_to_ios(to, data)
+      else
+        publish_to_android(to, data)
+      end
+
       body = {
         to: to,
         priority: 'high',
@@ -20,6 +26,40 @@ class FirebaseCloudMessagingApi
     end
 
     private
+
+    def publish_to_ios(to, data)
+      body = {
+        to: to,
+        notification: {
+          title: "",
+          body: "Pawoo", # iOSのNotification Share Extensionで書き換えている。iOS側でフックするために空文字列でない必要がある。
+        },
+        priority: 'high',
+        mutable_content: true,
+        data: data
+      }.to_json
+
+      build_client.post(
+        '/fcm/send',
+        body,
+        headers
+      )
+    end
+
+    def publish_to_android(to, data)
+      body = {
+        to: to,
+        priority: 'high',
+        content_available: true,
+        data: data
+      }.to_json
+
+      build_client.post(
+        '/fcm/send',
+        body,
+        headers
+      )
+    end
 
     def build_client
       Faraday::Connection.new(url: FCM_ENDPOINT) do |client|
