888274a3517bb6e06284f78dc97274cd616d0baf
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-12 18:57:40 +0900

    Optimize suggested_accounts

--- app/controllers/api/v1/suggested_accounts_controller.rb
+++ app/controllers/api/v1/suggested_accounts_controller.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 class Api::V1::SuggestedAccountsController < ApiController
+  DEFAULT_ACCOUNTS_LIMIT
+
   before_action -> { doorkeeper_authorize! :follow }
   before_action :require_user!
 
   respond_to :json
 
   def index
-    limit = limit_param(DEFAULT_ACCOUNTS_LIMIT)
+    limit = limit_param(15)
     seed = params[:seed] ? params[:seed].to_i : Random.new_seed
 
-    @accounts = suggested_accounts(current_user.account).shuffle(seed).per(limit).page(params[:page]).all
+    query = suggested_accounts(current_user.account).shuffle(seed).per(limit).page(params[:page])
+    @accounts = query.all
 
     media_attachments = popular_media_attachments(@accounts)
     @media_attachments_map = media_attachments.group_by(&:account_id)
 
-    next_path = api_v1_suggested_accounts_url(seed: seed, page: params[:page].to_i + 1) if @accounts.size == limit
+    next_path = api_v1_suggested_accounts_url(seed: seed, page: params[:page].to_i + 1) unless query.last_page?(params[:page])
     prev_path = api_v1_suggested_accounts_url(seed: seed, page: params[:page].to_i - 1) unless @accounts.empty?
     set_pagination_headers(next_path, prev_path)
   end
@@ -26,7 +29,7 @@ class Api::V1::SuggestedAccountsController < ApiController
   def popular_media_attachments(accounts)
     media_attachments_ids = accounts.map { |account|
       Rails.cache.fetch("suggested_account:published_attachments:#{account.id}") do
-        account.media_attachments.joins(:status).where(statuses: { sensitive: false, visibility: [:public, :unlisted] }).order(Status.arel_table[:favourites_count].desc).first(3).map(&:id)
+        MediaAttachment.joins(:status).where(account: account, statuses: { sensitive: false, visibility: [:public, :unlisted] }).order(Status.arel_table[:favourites_count].desc).first(3).map(&:id)
       end
     }.flatten
 
