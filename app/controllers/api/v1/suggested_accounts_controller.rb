0e4f1ef8a955a60b1c8a93fe83cc2ef14ee89f40
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-12 18:32:09 +0900

    Sort by seed

--- app/controllers/api/v1/suggested_accounts_controller.rb
+++ app/controllers/api/v1/suggested_accounts_controller.rb
@@ -8,15 +8,15 @@ class Api::V1::SuggestedAccountsController < ApiController
 
   def index
     limit = limit_param(DEFAULT_ACCOUNTS_LIMIT)
-    limit = 20
+    seed = params[:seed] ? params[:seed].to_i : Random.new_seed
 
-    @accounts = suggested_accounts(current_user.account).limit(limit)
+    @accounts = suggested_accounts(current_user.account).shuffle(seed).per(limit).page(params[:page]).all
 
     media_attachments = popular_media_attachments(@accounts)
     @media_attachments_map = media_attachments.group_by(&:account_id)
 
-    next_path = api_v1_suggested_accounts_url(exclude_ids: (excluded_ids + @accounts.ids).uniq.join(','))  if @accounts.size == limit
-    prev_path = api_v1_suggested_accounts_url(exclude_ids: excluded_ids[0..-(limit + 1)]) if @accounts.present? && excluded_ids.present?
+    next_path = api_v1_suggested_accounts_url(seed: seed, page: params[:page].to_i + 1) if @accounts.size == limit
+    prev_path = api_v1_suggested_accounts_url(seed: seed, page: params[:page].to_i - 1) unless @accounts.empty?
     set_pagination_headers(next_path, prev_path)
   end
 
@@ -37,7 +37,7 @@ class Api::V1::SuggestedAccountsController < ApiController
     following = account.following.ids
     muted_and_blocked = account.excluded_from_timeline_account_ids
 
-    SuggestedAccountQuery.exclude_ids([account.id] + excluded_ids + following + muted_and_blocked).all
+    SuggestedAccountQuery.exclude_ids([account.id] + excluded_ids + following + muted_and_blocked)
   end
 
   def excluded_ids
