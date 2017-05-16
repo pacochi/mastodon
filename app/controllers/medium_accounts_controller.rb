2cd3162f62be02fed18e25b386a4f11da031a38c
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-16 15:34:23 +0900

    Can not use `order(id: :desc)` and select(:id)

--- app/controllers/medium_accounts_controller.rb
+++ app/controllers/medium_accounts_controller.rb
@@ -5,7 +5,7 @@ class MediumAccountsController < ApplicationController
 
   def index
     @statuses = @account.statuses.permitted_for(@account, current_account).order('id desc').paginate_by_max_id(20, params[:max_id], params[:since_id])
-    status_ids = @statuses.joins(:media_attachments).distinct(:id).ids
+    status_ids = @statuses.joins(:media_attachments).distinct(:id).map(&:id)
     @statuses = @statuses.where(id: status_ids)
     @statuses = cache_collection(@statuses, Status)
   end
