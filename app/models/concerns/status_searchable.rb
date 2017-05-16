197654dbc437e49d274558f51654ed4097ac7839
fusshi- <fusshi-@pixiv.com>
2017-05-16 17:06:19 +0900

    公開トゥート投稿時のみ、トゥートをESに投稿

--- app/models/concerns/status_searchable.rb
+++ app/models/concerns/status_searchable.rb
@@ -5,7 +5,6 @@ module StatusSearchable
 
   included do
     include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
 
     index_name  "pawoo"
     document_type "status"
@@ -14,7 +13,7 @@ module StatusSearchable
     #mappings dynamic: 'false' do ... end
 
     def as_indexed_json(options = {})
-      if published?
+      if public_visibility?
         {
           id: id,
           text: text,
@@ -25,21 +24,19 @@ module StatusSearchable
     end
 
     after_commit on: [:create] do
-      __elasticsearch__.index_document if published?
-    end
-
-    after_commit on: [:update] do
-      __elasticsearch__.update_document if published?
+      Rails.logger.debug 'is toot public?'
+      Rails.logger.debug public_visibility?
+      if public_visibility?
+        Rails.logger.debug 'toot is sent to ES.@create'
+        __elasticsearch__.index_document
+      else
+        Rails.logger.debug 'toot is not sent to ES.@create'
+      end
     end
 
     after_commit on: [:destroy] do
       __elasticsearch__.delete_document
     end
-  end
-
-  def published?
-    :visibility == :public
-    true
   end
 
   class_methods do
