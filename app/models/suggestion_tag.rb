c4c24963d9877be218cb36d656114d566bde6960
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-16 17:01:25 +0900

    Refactor TrendTag

--- app/models/suggestion_tag.rb
+++ app/models/suggestion_tag.rb
@@ -4,6 +4,8 @@ class SuggestionTag < ApplicationRecord
   validates :order, :description, presence: true
   validates :tag_id, uniqueness: true
 
+  delegate :name, to: :tag, allow_nil: true
+
   def tag_attributes=(value)
     tag_name = (value || {})['name']
     self.tag = Tag.find_or_initialize_by(name: tag_name)
--- app/models/trend_tag.rb
+++ app/models/trend_tag.rb
@@ -3,19 +3,13 @@
 class TrendTag
   include ActiveModel::Model
 
-  MOCK_TREND = []
-
-  attr_reader :name, :description, :tag_type
-
-  def self.find_trend(limit = 5)
-    MOCK_TREND.first(limit)
-  end
-
+  attr_accessor :name, :description, :tag_type
 
   def self.find_suggestion(limit = 3)
     suggestion_tags = SuggestionTag.order(:order).preload(:tag).first(limit)
+
     suggestion_tags.map do |tag|
-      TrendTag.new(name: tag.tag.name, description: tag.description, tag_type: 'suggestion')
+      new(name: tag.name, description: tag.description, tag_type: 'suggestion')
     end
   end
 end
