486719aa8771b2594c2623fdbd1c655b2a325f4c
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-16 17:02:05 +0900

    Refactor Api::V1::TrendTagsController

--- app/controllers/api/v1/trend_tags_controller.rb
+++ app/controllers/api/v1/trend_tags_controller.rb
@@ -2,16 +2,11 @@
 
 class Api::V1::TrendTagsController < ApiController
   respond_to :json
-  before_action :limit_param!
 
   def index
-    @trend_tags = TrendTag.find_suggestion(@limit)
-  end
-
-  private
-
-  def limit_param!
     limit_size = params[:limit] || 5
-    @limit = limit_size.to_i.clamp(0, 5)
+    limit = limit_size.to_i.clamp(0, 5)
+
+    @trend_tags = TrendTag.find_suggestion(limit)
   end
 end
