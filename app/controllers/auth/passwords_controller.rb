93cd0cd877c1f0a0481ffd721d8d03c42db42568
ABCanG <abcang1015@gmail.com>
2017-05-17 16:18:41 +0900

    delete space

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Auth::PasswordsController < Devise::PasswordsController
-  skip_before_filter :require_no_authentication, only: [ :edit, :update ]
+  skip_before_filter :require_no_authentication, only: [:edit, :update]
 
   layout 'auth'
 end
