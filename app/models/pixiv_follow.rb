e47b8f9d522fdd909b1e20169e1a4ec8208b3034
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-17 19:12:56 +0900

    Create FetchPixivFollowsWorker

--- app/models/pixiv_follow.rb
+++ app/models/pixiv_follow.rb
@@ -3,4 +3,16 @@ class PixivFollow < ApplicationRecord
 
   validates :target_pixiv_uid, presence: true
   validates :oauth_authentication_id, uniqueness: { scope: :target_pixiv_uid }
+
+  def self.synchronize!(uids)
+    exists = pluck(:target_pixiv_uid)
+
+    deleted_uids = exists - uids
+    new_uid_attributes = (uids - exists).map { |uid| { target_pixiv_uid: uid } }
+
+    transaction do
+      where(target_pixiv_uid: deleted_uids).delete_all
+      create!(new_uid_attributes)
+    end
+  end
 end
---
+++ app/workers/fetch_pixiv_follows_worker.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class FetchPixivFollowsWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: 'pull'
+
+  def perform(oauth_authentication_id, access_token, refresh_token, expires_at)
+    client = PixivApi::Client.new(access_token: access_token, refresh_token: refresh_token, expires_at: expires_at)
+
+    uids = fetch_favorite_user_uids(client)
+
+    pixiv_follows = PixivFollow.where(oauth_authentication_id: oauth_authentication_id)
+    pixiv_follows.synchronize!(uids)
+  end
+
+  def fetch_favorite_user_uids(client)
+    page = 1
+    uids = []
+
+    while page
+      response = client.favorite_users(params: { count: 300, page: page })
+      uids += response.map(&:id)
+      page = response.pagination.next
+    end
+
+    uids.map(&:to_i)
+  end
+end
