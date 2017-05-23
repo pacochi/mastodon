084b494c00f86a482b1c30e4a1bf9f9e47407675
Shunsuke Michii <harukasan@me.com>
2017-05-23 21:12:51 +0900

    Make using pagenation at medium_accounts#index

--- app/controllers/medium_accounts_controller.rb
+++ app/controllers/medium_accounts_controller.rb
@@ -3,10 +3,16 @@
 class MediumAccountsController < ApplicationController
   include AccountControllerConcern
 
+  STATUSES_PER_PAGE = 20
+
   def index
-    @statuses = @account.statuses.permitted_for(@account, current_account).order('id desc').paginate_by_max_id(20, params[:max_id], params[:since_id])
-    status_ids = @statuses.joins(:media_attachments).distinct(:id).map(&:id)
-    @statuses = @statuses.where(id: status_ids)
-    @statuses = cache_collection(@statuses, Status)
+    @statuses = @account.statuses.permitted_for(@account, current_account).order('id desc').joins(:media_attachments).distinct(:id)
+    if params[:max_id].present? || params[:since_id].present?
+      @statuses = @statuses.paginate_by_max_id(STATUSES_PER_PAGE, params[:max_id], params[:since_id])
+    else
+      @statuses = @statuses.page(params[:page]).per(STATUSES_PER_PAGE).without_count
+    end
+
+    @statuses_collection = cache_collection(@statuses, Status)
   end
 end
