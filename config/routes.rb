eceb3c6675684e5627cd6131bcd46f17d6389f89
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-23 17:34:11 +0900

    Create Api::V1::OauthAuthenticationsController for pixiv app

--- config/routes.rb
+++ config/routes.rb
@@ -167,6 +167,7 @@ Rails.application.routes.draw do
       resources :pixiv_twitter_images, only: [:create]
       resources :firebase_cloud_messaging_tokens, only: [:create, :destroy], param: :platform
       resources :suggested_accounts, only: [:index]
+      resources :oauth_authentications, only: [:show], param: :uid
 
       resource :instance, only: [:show]
 
