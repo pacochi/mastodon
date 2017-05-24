17f85abcee87b15f1f77cce2c6995390ce08e61d
Shunsuke Michii <harukasan@me.com>
2017-05-24 12:17:26 +0900

    Allow max_id

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,12 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by!(name: params[:id].downcase)
-    @statuses = Status.as_tag_timeline(@tag, current_account, params[:local]).page(params[:page]).per(STATUSES_PER_PAGE)
+    @statuses = Status.as_tag_timeline(@tag, current_account, params[:local])
+    if params[:max_id].present? || params[:since_id].present?
+      @statuses = @statuses.paginate_by_max_id(STATUSES_PER_PAGE, params[:max_id], params[:since_id])
+    else
+      @statuses = @statuses.page(params[:page]).per(STATUSES_PER_PAGE).without_count
+    end
     @statuses_collection = cache_collection(@tag.nil? ? [] : @statuses, Status)
   end
 end
