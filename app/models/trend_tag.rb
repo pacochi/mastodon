d0757815453837abd7f08bf9780e6f3b82c86e51
orekyuu <orekyuu@pixiv.com>
2017-05-25 19:02:35 +0900

    Kill raw sql.

--- app/models/trend_tag.rb
+++ app/models/trend_tag.rb
@@ -2,14 +2,25 @@
 
 class TrendTag
   include ActiveModel::Model
+  TREND_CURRENT_KEY = 'trend_current'
 
   attr_accessor :name, :description, :tag_type
 
-  def self.find_suggestion(limit = 3)
-    suggestion_tags = SuggestionTag.order(:order).preload(:tag).first(limit)
+  def self.find_tags(limit = 3)
+    trend_cache = Redis.current.get(TrendTag::TREND_CURRENT_KEY)
+    trend_tags = []
 
-    suggestion_tags.map do |tag|
+    if trend_cache.present?
+      trend_tags = JSON.parse(trend_cache, {:symbolize_names => true}).take(3).map do |tag|
+        # NOTE: 本来はdescriptionにトゥート件数を表示したいが、現状表示するほどトゥートがないので空を出している
+        new(name: tag[:tag_name], description: '', tag_type: 'trend')
+      end
+    end
+
+    suggestion_tags = SuggestionTag.order(:order).preload(:tag).first(limit).map do |tag|
       new(name: tag.name, description: tag.description, tag_type: 'suggestion')
     end
+
+    trend_tags.concat(suggestion_tags).dup(&:name).take(limit)
   end
 end
