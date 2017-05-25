3b9ac4bb3c1d983711cc382b6c8cd519a84d0e06
fusshi- <fusshi-@pixiv.com>
2017-05-25 10:31:35 +0900

    不要なESの設定を削除

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,8 +3,6 @@ Elasticsearch::Model.client = Elasticsearch::Client.new(
     {
       host: ENV['ELASTICSEARCH_HOST'] || '192.168.42.1',
       port: ENV['ELASTICSEARCH_PORT'] || '9200',
-      user: ENV['ELASTICSEARCH_USER'] || 'elastic',
-      password: ENV['ELASTICSEARCH_PASS'] || 'changeme'
     }
   ]
 )
