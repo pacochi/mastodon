7a1d70b980b16580f817c2c9f72859c942f792bc
orekyuu <orekyuu@pixiv.com>
2017-05-25 11:29:54 +0900

    Add unique index.

--- db/migrate/20170524042615_create_trend_ng_words.rb
+++ db/migrate/20170524042615_create_trend_ng_words.rb
@@ -3,8 +3,9 @@ class CreateTrendNgWords < ActiveRecord::Migration[5.0]
     create_table :trend_ng_words do |t|
       t.string :word, null: false, default: ''
       t.string :memo, null: false, default: ''
-
       t.timestamps
+
+      t.index :word, unique: true
     end
   end
 end
