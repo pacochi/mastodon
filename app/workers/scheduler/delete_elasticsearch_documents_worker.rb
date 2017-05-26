87e065fdbaf10f462cb439768750ac19c92c1d3b
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-26 17:12:56 +0900

    Update delete_elasticsearch_documents_worker.rb

--- app/workers/scheduler/delete_elasticsearch_documents_worker.rb
+++ app/workers/scheduler/delete_elasticsearch_documents_worker.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Scheduler::DeleteElasticsearchDocumentsWorker
-  EXPIRE_BEFORE = 7.days
+  EXPIRE_BEFORE = 14.days
 
   def perform
     Elasticsearch::Model.client.delete_by_query(
