fef94d1c649fe8dce996f3307acbce476c2c7177
orekyuu <orekyuu@pixiv.com>
2017-05-31 15:55:18 +0900

    wip

--- app/controllers/api/v1/music_controller.rb
+++ app/controllers/api/v1/music_controller.rb
@@ -1,17 +1,24 @@
 # frozen_string_literal: true
 
 class Api::V1::MusicController < ApiController
-  before_action -> { doorkeeper_authorize! :write }
-  before_action :require_user!
-
-  # include ObfuscateFilename
-  # obfuscate_filename :file
+  # before_action -> { doorkeeper_authorize! :write }
+  # before_action :require_user!
 
   respond_to :json
 
   def create
-    # TODO: 後でやる
-    @media = MediaAttachment.create!(account: current_user.account, file: media_params[:file], type: MediaAttachment.types[:music])
+    mp4 = MusicConvertService.new.call(media_params[:title], media_params[:artist], media_params[:music], media_params[:picture])
+    @media = MediaAttachment.create!(account: current_user.account, file: mp4, type: MediaAttachment.types[:music])
+  rescue Paperclip::Error
+    #TODO: エラーメッセージが適切でないのでいいかんじにする
+    render json: { error: 'Error processing thumbnail for uploaded media' }, status: 500
+  end
+
+  private
+
+  def media_params
+    # params.permit(:music, :image, :title, :artist)
+    params
   end
 
 end
--- app/services/music_convert_service.rb
+++ app/services/music_convert_service.rb
@@ -1,9 +1,32 @@
+require 'mp3info'
+require 'streamio-ffmpeg'
+require 'tempfile'
+
 class MusicConvertService < BaseService
   def call(title, artist, music_file, image_file)
     # ffmpeg generates 60 extra silent frames at the end when you generate mp4 from png and mp3
     # we get the length of the mp3 file and trim the output to circumvent this issue
-    music_length = Mp3Info.open(file.path, &:length)
-    # TODO: 色々頑張ってmp4を戻せるようにする
-    results # mp4
+    music_length = Mp3Info.open(music_file.path, &:length)
+
+    options = {
+      video_codec: 'libx264',
+      resolution: '320x240',
+      video_bitrate: 300,
+      audio_codec: 'libmp3lame',
+      audio_bitrate: 192,
+      audio_sample_rate: 44100,
+      audio_channels: 2,
+      duration: music_length.to_i,
+      custom: ['-i', FFMPEG::Movie.new(music_file.path).path, '-metadata', "title=#{title}", '-metadata', "artist=#{artist}"],
+    }
+
+    # TODO: outputをTempfileにする
+    mp4 = FFMPEG::Transcoder.new(nil, '/Users/orekyuu/Desktop/test.mp4',
+                                 options,
+                                 input: image_file.path,
+                                 input_options: { loop: '1', r: '4' }
+    ).run
+    raise 'おらおら'
+
   end
 end
