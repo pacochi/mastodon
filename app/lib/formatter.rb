b50d9944acec378bc09b47ac9632156ef60e8d35
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 11:59:27 +0900

    Resolve a merged conflict - app/lib/formatter.rb

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -17,12 +17,6 @@ class Formatter
       prepend_reblog = false
     end
 
-<<<<<<< HEAD
-    html = status.text
-    html = encode_and_link_urls(html, status.mentions)
-
-    html = simple_format(html, {}, sanitize: false)
-=======
     raw_content = status.public_send(attribute)
 
     return '' if raw_content.blank?
@@ -35,7 +29,6 @@ class Formatter
     html = "RT @#{prepend_reblog} #{html}" if prepend_reblog
     html = encode_and_link_urls(html, linkable_accounts)
     html = simple_format(html, {}, sanitize: false) if paragraphize
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
     html = html.delete("\n")
 
     html.html_safe # rubocop:disable Rails/OutputSafety
@@ -43,17 +36,6 @@ class Formatter
 
   def reformat(html)
     sanitize(html, Sanitize::Config::MASTODON_STRICT).html_safe # rubocop:disable Rails/OutputSafety
-  end
-
-  def format_spoiler(status)
-    return reformat(status.spoiler_text) unless status.local?
-
-    html = status.spoiler_text
-    html = encode(html)
-    html = html.delete("\n")
-    html = link_hashtags(html)
-
-    html.html_safe # rubocop:disable Rails/OutputSafety
   end
 
   def plaintext(status)
@@ -85,7 +67,6 @@ class Formatter
     HTMLEntities.new.encode(html)
   end
 
-<<<<<<< HEAD
   def strip_urls(text)
     text.dup.tap do |dupped|
       entities = Extractor.extract_entities_with_indices(text, extract_url_without_protocol: false)
@@ -93,23 +74,6 @@ class Formatter
     end
   end
 
-  def encode_and_link_urls(html, mentions = nil)
-    entities = Extractor.extract_entities_with_indices(html, extract_url_without_protocol: false)
-
-    rewrite(html.dup, entities) do |entity|
-      if entity[:url]
-        link_to_url(entity)
-      elsif entity[:hashtag]
-        link_to_hashtag(entity)
-      elsif entity[:screen_name]
-        link_to_mention(entity, mentions)
-      end
-    end
-  end
-
-  def rewrite(text, entities)
-    chars = text.to_s.to_char_a
-=======
   def encode_and_link_urls(html, accounts = nil)
     entities = Extractor.extract_entities_with_indices(html, extract_url_without_protocol: false)
 
@@ -134,34 +98,13 @@ class Formatter
     end
 
     result = []
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
 
-    # sort by start index
-    entities = entities.sort_by do |entity|
-      indices = entity.respond_to?(:indices) ? entity.indices : entity[:indices]
-      indices.first
-    end
-
-    result = []
     last_index = entities.reduce(0) do |index, entity|
       indices = entity.respond_to?(:indices) ? entity.indices : entity[:indices]
       result << encode(chars[index...indices.first].join)
       result << yield(entity)
       indices.last
     end
-<<<<<<< HEAD
-    result << encode(chars[last_index..-1].join)
-
-    result.flatten.join
-  end
-
-  def link_to_url(entity)
-    normalized_url = Addressable::URI.parse(entity[:url]).normalize
-    html_attrs = {
-      target: '_blank',
-      rel: 'nofollow noopener',
-    }
-=======
 
     result << encode(chars[last_index..-1].join)
 
@@ -172,28 +115,11 @@ class Formatter
     normalized_url = Addressable::URI.parse(entity[:url]).normalize
     html_attrs     = { target: '_blank', rel: 'nofollow noopener' }
 
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
     Twitter::Autolink.send(:link_to_text, entity, link_html(entity[:url]), normalized_url, html_attrs)
   rescue Addressable::URI::InvalidURIError
     encode(entity[:url])
   end
 
-<<<<<<< HEAD
-  def link_to_mention(entity, mentions)
-    acct = entity[:screen_name]
-    return link_to_account(acct) unless mentions
-    mention = mentions.find { |item| TagManager.instance.same_acct?(item.account.acct, acct) }
-    mention ? mention_html(mention.account) : "@#{acct}"
-  end
-
-  def link_to_account(acct)
-    username, domain = acct.split('@')
-    domain = nil if TagManager.instance.local_domain?(domain)
-    account = Account.find_remote(username, domain)
-    account ? mention_html(account) : "@#{acct}"
-  end
-
-=======
   def link_to_mention(entity, linkable_accounts)
     acct = entity[:screen_name]
 
@@ -212,7 +138,6 @@ class Formatter
     account ? mention_html(account) : "@#{acct}"
   end
 
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
   def link_to_hashtag(entity)
     hashtag_html(entity[:hashtag])
   end
@@ -228,11 +153,7 @@ class Formatter
   end
 
   def hashtag_html(tag)
-<<<<<<< HEAD
-    "<a href=\"#{tag_url(tag.downcase)}\" class=\"mention hashtag\">#<span>#{tag}</span></a>"
-=======
     "<a href=\"#{tag_url(tag.downcase)}\" class=\"mention hashtag\" rel=\"tag\">#<span>#{tag}</span></a>"
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
   end
 
   def mention_html(account)
