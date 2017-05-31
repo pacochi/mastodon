e71ae9434a44204ba5e0ad4ce952b4a68b8c7f69
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 16:33:19 +0900

    Resolve a merged conflict - app/javascript/mastodon/features/compose/containers/search_results_container.js

--- app/javascript/mastodon/features/compose/containers/search_results_container.js
+++ app/javascript/mastodon/features/compose/containers/search_results_container.js
@@ -3,11 +3,8 @@ import SearchResults from '../components/search_results';
 
 const mapStateToProps = state => ({
   results: state.getIn(['search', 'results']),
-<<<<<<< HEAD:app/assets/javascripts/components/features/compose/containers/search_results_container.jsx
   isAdmin: state.getIn(['meta', 'is_user_admin']),
-  searchKeyword: state.getIn(['search', 'value'])
-=======
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc:app/javascript/mastodon/features/compose/containers/search_results_container.js
+  searchKeyword: state.getIn(['search', 'value']),
 });
 
 export default connect(mapStateToProps)(SearchResults);
