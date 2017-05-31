b507bb0fce55f2c398dc28299ef6da135b7b177d
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 15:32:19 +0900

    Resolve a merged conflict - app/helpers/admin/filter_helper.rb

--- app/helpers/admin/filter_helper.rb
+++ app/helpers/admin/filter_helper.rb
@@ -4,13 +4,11 @@ module Admin::FilterHelper
   ACCOUNT_FILTERS = %i(local remote by_domain silenced suspended recent username display_name email ip).freeze
   REPORT_FILTERS = %i(resolved account_id target_account_id).freeze
 
-  FILTERS = (FORM_ACCOUNT_FILTERS + REPORT_FILTERS).freeze
+  FILTERS = ACCOUNT_FILTERS + REPORT_FILTERS
 
-  def filter_link_to(text, new_params)
-    url = url_for(filtered_params(new_params))
-    klass = 'selected' if filtered_params(params) == filtered_params(new_params)
-
-    link_to(text, url, class: klass)
+  def filter_link_to(text, more_params)
+    new_url = filtered_url_for(more_params)
+    link_to text, new_url, class: filter_link_class(new_url)
   end
 
   def table_link_to(icon, text, path, options = {})
