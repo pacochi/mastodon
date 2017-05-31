0fe9da4a7acfe6aaedb01d2dca03f018bf73fb26
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 18:32:27 +0900

    Resolve a merged conflict - app/javascript/mastodon/features/home_timeline/index.js

--- app/javascript/mastodon/features/home_timeline/index.js
+++ app/javascript/mastodon/features/home_timeline/index.js
@@ -32,7 +32,7 @@ class HomeTimeline extends React.PureComponent {
     if (hasFollows) {
       emptyMessage = <FormattedMessage id='empty_column.home.inactivity' defaultMessage="Your home feed is empty. If you have been inactive for a while, it will be regenerated for you soon." />;
     } else {
-      emptyMessage = <FormattedMessage id='empty_column.home' defaultMessage="You aren't following anyone yet. Visit {public} or use search to get started and meet other users." values={{ public: <Link to='/timelines/public'><FormattedMessage id='empty_column.home.public_timeline' defaultMessage='the public timeline' /></Link> }} />;
+      emptyMessage = <FormattedMessage id='empty_column.home' defaultMessage="You aren't following anyone yet. Visit {public} or use search to get started and meet other users." values={{ public: <Link to='/suggested_accounts'><FormattedMessage id='empty_column.home.suggested_accounts' defaultMessage='the active accounts' /></Link> }} />;
     }
 
     return (
