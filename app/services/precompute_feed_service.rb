b8e9eb3b898b6fff77221eb22816795d1c60dcd0
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 14:56:57 +0900

    Resolve a merged conflict - app/services/precompute_feed_service.rb

--- app/services/precompute_feed_service.rb
+++ app/services/precompute_feed_service.rb
@@ -14,15 +14,8 @@ class PrecomputeFeedService < BaseService
 
   def populate_feed
     redis.pipelined do
-<<<<<<< HEAD
-      # NOTE: Added `id desc, account_id desc` to `ORDER BY` section to optimize query.
-      Status.as_home_timeline(account).order(account_id: :desc).limit(FeedManager::MAX_ITEMS / 4).each do |status|
-        next if status.direct_visibility? || FeedManager.instance.filter?(:home, status, account.id)
-        redis.zadd(FeedManager.instance.key(:home, account.id), status.id, status.reblog? ? status.reblog_of_id : status.id)
-=======
       statuses.each do |status|
         process_status(status)
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
       end
     end
   end
