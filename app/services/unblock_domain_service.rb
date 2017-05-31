a2bf4a2fb6009021cdf9152b44b9508598c9d1e0
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-31 14:58:29 +0900

    Resolve a merged conflict - app/services/unblock_domain_service.rb

--- app/services/unblock_domain_service.rb
+++ app/services/unblock_domain_service.rb
@@ -1,20 +1,7 @@
 # frozen_string_literal: true
 
 class UnblockDomainService < BaseService
-<<<<<<< HEAD
-  def call(domain_block, retroactive)
-    if retroactive
-      accounts = Account.where(domain: domain_block.domain).in_batches
-
-      if domain_block.silence?
-        accounts.update_all(silenced: false)
-      else
-        accounts.update_all(suspended: false)
-      end
-    end
-=======
   attr_accessor :domain_block
->>>>>>> 8963f8c3c2630bfcc377a5ca0513eef5a6b2a4bc
 
   def call(domain_block, retroactive)
     @domain_block = domain_block
