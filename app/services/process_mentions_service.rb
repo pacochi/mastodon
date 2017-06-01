9191049f5571ce5828905e0ab8d453a75a79fcf7
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-01 17:26:22 +0900

    Fixes rspec error

--- app/services/process_mentions_service.rb
+++ app/services/process_mentions_service.rb
@@ -27,7 +27,7 @@ class ProcessMentionsService < BaseService
       mentioned_account.mentions.where(status: status).first_or_create(status: status)
     end
 
-    time_limit = TimeLimit.from_tags(status)
+    time_limit = TimeLimit.from_tags(status.tags)
 
     status.mentions.includes(:account).each do |mention|
       mentioned_account = mention.account
