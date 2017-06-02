e458b35c0b3aed9045e953cf74f6c618e0a1bcfd
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-02 20:37:55 +0900

    Compile assets with webpack

--- config/webpack/loaders/assets.js
+++ config/webpack/loaders/assets.js
@@ -1,12 +1,23 @@
+const path = require('path');
 const { env, publicPath } = require('../configuration.js');
 
+const imagePath = path.join('app', 'javascript', 'images');
+const imagePathRegexp = new RegExp(`${imagePath}/`);
+
 module.exports = {
-  test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
+  test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2|mp4)$/i,
   use: [{
     loader: 'file-loader',
     options: {
       publicPath,
-      name: env.NODE_ENV === 'production' ? '[name]-[hash].[ext]' : '[name].[ext]',
+      name: env.NODE_ENV === 'production' ? '[path][name]-[hash].[ext]' : '[path][name].[ext]',
+      outputPath(url) {
+        if (imagePathRegexp.test(url)) {
+          return url.replace(imagePathRegexp, '');
+        } else {
+          return path.basename(url);
+        }
+      },
     },
   }],
 };
