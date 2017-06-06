c75ba477b400494dd6b8a9b1845e68977c010e29
ABCanG <abcang1015@gmail.com>
2017-06-06 14:48:12 +0900

    refactor queue_item

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -4,26 +4,29 @@ class QueueItem
   include ActiveModel::Model
   include ActiveModel::Serialization
 
-  attr_accessor :id, :info, :thumbnail_url, :music_url, :video_url, :duration, :link, :source_type, :account
+  attr_accessor :id, :info, :thumbnail_url, :music_url, :video_url, :duration, :link, :source_type, :account_id
 
   class << self
     include RoutingHelper
     include HttpHelper
 
-    def create(link, account)
-      #todo linkをパースしてAddQueueItemを作る
-      ret = pawoo_link(link, account)
-      return ret if ret
-      booth_link(link, account)
+    def create_from_link(deck, link, account)
+      # TODO: 回数制限チェック
+
+      # TODO: linkをパースしてAddQueueItemを作る
+      item = pawoo_link(link, account) || booth_link(link, account) # || youtube_link(link, account)
+      Redis.current.set("music:playlist:#{deck}:#{item.id}", item.to_json) if item
+
+      item
     end
 
     private
 
     def pawoo_link(link, account)
-      domain = 'localhost:3000'
-      #todo redisのキャッシュもシュッとやる
-      status_id = find_status_id(link, %r{http://#{domain}/web/statuses/(\d+)})
-      status_id = find_status_id(link, %r{http://#{domain}/@\W+/(\d+)}) if status_id
+      status_id = find_status_id(link)
+
+      cache = Redis.current.get("music:link:pawoo:#{status_id}")
+      return new(JSON.parse(cache, symbolize: true)) if cache
 
       return nil unless status_id
       video = MediaAttachment.find_by(status_id: status_id, type: MediaAttachment.types[:video])
@@ -31,50 +34,51 @@ class QueueItem
 
       video_url = full_asset_url(video.file.url(:original))
 
-      #todo mp3infoでシュッっとやる
-
-      QueueItem.new(
-        id: SecureRandom.uuid, title: 'title',
-        artist: 'aaaa',
-        thumbnail_url: '',
-        music_url: '',
+      item = new(
+        id: SecureRandom.uuid,
+        info: 'title - artist', # TODO: DBからinfo取ってくる
+        thumbnail_url: nil,
+        music_url: nil,
         video_url: video_url,
         link: link,
-        duration: 0,
-        source_type: 'pawoo', account: account.id
+        duration: 0, # TODO: DBからinfo取ってくる
+        source_type: 'pawoo',
+        account_id: account.id
       )
+
+      Redis.current.setex("music:link:pawoo:#{status_id}", 3600, item.to_json)
+      item
     end
 
-    def find_status_id(link, reg)
-      matched = link.match(reg)
-      matched ? matched[1] : nil
+    def find_status_id(link)
+      domain = 'localhost:3000' # TODO: 直す
+      matched = link.match(%r{https://pawoo.net/(@\W+)|(web/statuses)/(?<status_id>\d+)})
+      matched ? matched[:status_id] : nil
     end
 
     def booth_link(link, account)
-
       shop_id = find_shop_id(link)
       return nil unless shop_id
 
-      cache = Redis.current.get("booth-kink:#{shop_id}")
-      if cache
-        cache = JSON.parse(cache, symbolize: true)
-        return QueueItem.new(cache)
-      end
+      cache = Redis.current.get("music:link:booth:#{shop_id}")
+      return new(JSON.parse(cache, symbolize: true)) if cache
 
       json = JSON.parse(http_client.get("https://api.booth.pm/pixiv/items/#{shop_id}").body.to_s)
-      item = QueueItem.new(
+      # TODO: 音楽かどうかの判定
+      user_or_shop_name = json['body']['shop']['user']['nickname'] || json['body']['shop']['name']
+      item = new(
         id: SecureRandom.uuid,
-        info: "#{json['body']['name']} #{json['body']['shop']['user']['nickname']}",
+        info: "#{json['body']['name']} - #{user_or_shop_name}",
         thumbnail_url: json['body']['primary_image']['url'],
         music_url: json['body']['sound']['long_url'],
-        video_url: '',
+        video_url: nil,
         duration: json['body']['sound']['duration'],
         link: link,
         source_type: 'booth',
-        account: account.id,
+        account_id: account.id,
       )
 
-      Redis.current.set("booth-kink:#{shop_id}", item.to_json)
+      Redis.current.setex("music:link:booth:#{shop_id}", 3600, item.to_json)
       item
     end
 
@@ -82,5 +86,15 @@ class QueueItem
       matched = link.match(%r{https://booth.pm/ja/items/(\d+)})
       matched ? matched[1] : nil
     end
+
+    def youtube_link(link, account)
+      # TODO:
+    end
+
+    def skip(deck, account)
+      # TODO:
+      true
+    end
+
   end
 end
