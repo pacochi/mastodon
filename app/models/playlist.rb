7759cee12cd537ae5193e410047416b8275ceaf5
orekyuu <orekyuu@pixiv.com>
2017-06-08 15:38:15 +0900

    Playlistの操作回数制限を実装

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,8 +3,8 @@
 class Playlist
 
   MAX_QUEUE_SIZE = 10
-  MAX_ADD_COUNT = 2
-  MAX_SKIP_COUNT = 1
+  MAX_ADD_COUNT = 3
+  MAX_SKIP_COUNT = 3
   attr_accessor :deck
 
   def initialize(deck)
@@ -12,11 +12,7 @@ class Playlist
   end
 
   def add(link, account)
-    # TODO: 回数チェック
-    # last_one_hour = Time.current - 1.hour...Time.current
-    # if MAX_QUEUE_SIZE < queue_items.size || MAX_ADD_COUNT < ControlLog.where(account: account, type: 'add_queue_item', created_at: last_one_hour).count
-    #   return nil
-    # end
+    return nil unless check_count(music_add_count_key(account))
 
     queue_item = QueueItem.create_from_link(link, account)
     if queue_item && redis_push(queue_item)
@@ -29,11 +25,7 @@ class Playlist
   end
 
   def skip(id, account)
-    # TODO: 回数チェック
-    # last_one_hour = Time.current - 1.hour...Time.current
-    # if MAX_SKIP_COUNT < ControlLog.where(account: account, type: 'skip_queue_item', created_at: last_one_hour).count
-    #   return nil
-    # end
+    return nil unless check_count(music_skip_count_key(account))
 
     self.next(id)
   end
@@ -122,6 +114,42 @@ class Playlist
         nil
       end
     end
+  end
+
+  def music_add_count_key(account)
+    "music:playlist:add-count:#{account.id}"
+  end
+
+  def music_skip_count_key(account)
+    "music:playlist:skip-count:#{account.id}"
+  end
+
+  def check_count(key)
+    redis_watch(key) do
+      count = redis.get(key)&.to_i || 0
+      ttl = redis.ttl(key)
+      ttl = 60 * 60 if ttl <= 0
+
+      return false unless count < MAX_ADD_COUNT
+
+      redis.setex(key, ttl, count + 1)
+      true
+    end
+
+  end
+
+  def redis_watch(keys, retry_count = 3)
+    while retry_count.positive?
+      begin
+        redis.watch(keys) do
+          return yield()
+        end
+      ensure
+        redis.unwatch
+        retry_count -= 1
+      end
+    end
+    false
   end
 
   def redis
