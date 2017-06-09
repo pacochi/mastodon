f6c1c28622c3a5fbaae83d6cfa6f554d32e74a0a
Takuma Hanatani <mail@potato4d.me>
2017-06-09 10:51:05 +0900

    fix streaming

--- app/assets/javascripts/components/features/ui/containers/play_control_container.jsx
+++ app/assets/javascripts/components/features/ui/containers/play_control_container.jsx
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import PlayControl from '../components/play_control';
 
 const mapStateToProps = state => ({
-  accessToken: state.getIn(['meta', 'access_token'])
-  streamingAPIBaseURL: state.getIn(['meta', 'streaming_api_base_url']),
+  accessToken: state.getIn(['meta', 'access_token']),
+  streamingAPIBaseURL: state.getIn(['meta', 'streaming_api_base_url'])
 });
 
 const mapDispatchToProps = dispatch => ({
