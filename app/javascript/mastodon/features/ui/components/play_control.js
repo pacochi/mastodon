53dfe8f7b674e5dca06aaa12be345d8e190be8f6
potato4d <mail@potato4d.me>
2017-06-13 17:04:26 +0900

    Create play_control.js

--- app/javascript/mastodon/features/ui/components/play_control.js
+++ app/javascript/mastodon/features/ui/components/play_control.js
@@ -402,7 +402,7 @@ class MusicPlayer extends React.PureComponent {
                                 <img src="/player/logos/pawoo-music.svg" />
                                 <div className='platform-info'>
                                   <div className='platform-info__title'>Pawoo Music</div>
-                                  <div className='platform-info__url'>http://pawoo.net/web/statuses/[XXXXX…]</div>
+                                  <div className='platform-info__url'>https://music.pawoo.net/web/statuses/[XXXXX…]</div>
                                 </div>
                               </li>
                               <li>
