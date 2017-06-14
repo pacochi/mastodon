85925fa3727e7ad83ddb575a15934b3fbd7da17a
ABCanG <abcang1015@gmail.com>
2017-06-14 10:24:01 +0900

    change playlist error message

--- app/controllers/api/v1/deck_queues_controller.rb
+++ app/controllers/api/v1/deck_queues_controller.rb
@@ -11,30 +11,30 @@ class Api::V1::DeckQueuesController < ApiController
     if @playlist.add(params[:link], current_user.account)
       render_empty
     else
-      render json: { error: 'エラー' }, status: :service_unavailable
+      render json: { error: '不明なエラーが発生しました。' }, status: :service_unavailable
     end
   rescue Mastodon::MusicSourceNotFoundError => _
-    render json: { error: '無効なURL' }, status: :bad_request
+    render json: { error: '動画を追加できません。 （？）ボタンから、対応サービスを確認ください。' }, status: :bad_request
   rescue Mastodon::PlayerControlLimitError => _
-    render json: { error: '操作回数制限' }, status: :too_many_requests
+    render json: { error: "１時間に楽曲を追加できる回数は#{Playlist::MAX_ADD_COUNT}回までです。"}, status: :too_many_requests
   rescue Mastodon::PlaylistSizeOverError => _
-    render json: { error: 'プレイリストのサイズ制限オーバー' }, status: :bad_request
+    render json: { error: 'プレイリストにこれ以上曲を追加できません。' }, status: :bad_request
   rescue Mastodon::RedisMaxRetryError => _
-    render json: { error: 'エラー' }, status: :service_unavailable
+    render json: { error: '不明なエラーが発生しました。' }, status: :service_unavailable
   end
 
   def destroy
     if @playlist.skip(params[:id], current_user.account)
       render_empty
     else
-      render json: { error: 'エラー' }, status: :service_unavailable
+      render json: { error: '不明なエラーが発生しました。' }, status: :service_unavailable
     end
   rescue Mastodon::PlayerControlLimitError => _
-    render json: { error: '操作回数制限' }, status: :too_many_requests
+    render json: { error: "１時間にスキップできる回数は#{Playlist::MAX_SKIP_COUNT}回までです。" }, status: :too_many_requests
   rescue Mastodon::PlaylistEmptyError => _
-    render json: { error: 'プレイリストが空' }, status: :bad_request
+    render json: { error: 'スキップに失敗しました。' }, status: :bad_request
   rescue Mastodon::RedisMaxRetryError => _
-    render json: { error: 'エラー' }, status: :service_unavailable
+    render json: { error: '不明なエラーが発生しました。' }, status: :service_unavailable
   end
 
   private
