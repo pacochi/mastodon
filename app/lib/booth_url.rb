047a543c7c844b06c808188f64fdc8f334e07768
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-14 19:28:09 +0900

    Extract item_id with BoothUrl.extract_booth_item_id

--- app/lib/booth_url.rb
+++ app/lib/booth_url.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class BoothUrl
+  APOLLO_URL = %r{https?://booth\.pm/apollo/a\d+/item\?.*id=(?<item_id>\d+)}
+  BOOTH_URL = %r{https://(?:[a-z0-9][a-z0-9\-]+[a-z0-9]\.booth\.pm|booth\.pm/(?:zh-tw|zh-cn|ko|ja|en))/items/(?<item_id>\d+)}
+
+  def self.extract_booth_item_id(text)
+    entities = Extractor.extract_entities_with_indices(text, extract_url_without_protocol: false)
+    urls = entities.map { |entry| entry[:url] }
+
+    item_ids = urls.map do |url|
+      id = url.match(APOLLO_URL).try(:[], :item_id) || url.match(BOOTH_URL).try(:[], :item_id)
+      id.to_i if id
+    end
+
+    item_ids.first
+  end
+
   def initialize(uri)
     @uri = uri
   end
