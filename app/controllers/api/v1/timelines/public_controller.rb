2ccb62ca3c6f87476765d20d8f30cc4463f42558
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-15 12:27:07 +0900

    Resolve a merged conflict - app/controllers/api/v1/timelines/public_controller.rb

--- app/controllers/api/v1/timelines/public_controller.rb
+++ app/controllers/api/v1/timelines/public_controller.rb
@@ -12,26 +12,9 @@ class Api::V1::Timelines::PublicController < Api::BaseController
 
   private
 
-<<<<<<< HEAD
-    def public_statuses
-      statuses = public_timeline_statuses.paginate_by_max_id(
-        limit_param(DEFAULT_STATUSES_LIMIT),
-        params[:max_id],
-        params[:since_id]
-      )
-
-      if params[:media]
-        # `SELECT DISTINCT id, updated_at` is too slow, so pluck ids at first, and then select id, updated_at with ids.
-        status_ids = statuses.joins(:media_attachments).distinct(:id).pluck(:id)
-        statuses.where(id: status_ids)
-      else
-        statuses
-      end
-=======
   def load_statuses
     cached_public_statuses.tap do |statuses|
       set_maps(statuses)
->>>>>>> 947887f261f74f84312327a5265553e8f16655fe
     end
   end
 
@@ -39,21 +22,20 @@ class Api::V1::Timelines::PublicController < Api::BaseController
     cache_collection public_statuses, Status
   end
 
-<<<<<<< HEAD
-    def next_path
-      api_v1_timelines_public_url pagination_params(max_id: @statuses.last.id, media: params[:media])
-    end
-
-    def prev_path
-      api_v1_timelines_public_url pagination_params(since_id: @statuses.first.id, media: params[:media])
-    end
-=======
   def public_statuses
-    public_timeline_statuses.paginate_by_max_id(
+    statuses = public_timeline_statuses.paginate_by_max_id(
       limit_param(DEFAULT_STATUSES_LIMIT),
       params[:max_id],
       params[:since_id]
     )
+
+    if params[:media]
+      # `SELECT DISTINCT id, updated_at` is too slow, so pluck ids at first, and then select id, updated_at with ids.
+      status_ids = statuses.joins(:media_attachments).distinct(:id).pluck(:id)
+      statuses.where(id: status_ids)
+    else
+      statuses
+    end
   end
 
   def public_timeline_statuses
@@ -69,11 +51,11 @@ class Api::V1::Timelines::PublicController < Api::BaseController
   end
 
   def next_path
-    api_v1_timelines_public_url pagination_params(max_id: pagination_max_id)
+    api_v1_timelines_public_url pagination_params(max_id: pagination_max_id, media: params[:media])
   end
 
   def prev_path
-    api_v1_timelines_public_url pagination_params(since_id: pagination_since_id)
+    api_v1_timelines_public_url pagination_params(since_id: pagination_since_id, media: params[:media])
   end
 
   def pagination_max_id
@@ -82,6 +64,5 @@ class Api::V1::Timelines::PublicController < Api::BaseController
 
   def pagination_since_id
     @statuses.first.id
->>>>>>> 947887f261f74f84312327a5265553e8f16655fe
   end
 end
