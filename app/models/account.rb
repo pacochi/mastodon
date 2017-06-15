dc8167281573623d8e84ebde1d83f4a9383078bf
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-15 13:25:16 +0900

    Resolve a merged conflict - app/models/account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -165,32 +165,7 @@ class Account < ApplicationRecord
   end
 
   class << self
-<<<<<<< HEAD
-    def find_local!(username)
-      find_remote!(username, nil)
-    end
-
-    def find_remote!(username, domain)
-      raise ActiveRecord::RecordNotFound if username.blank?
-      where('lower(accounts.username) = ?', username.downcase).where(domain.nil? ? { domain: nil } : 'lower(accounts.domain) = ?', domain&.downcase).take!
-    end
-
-    def find_local(username)
-      find_local!(username)
-    rescue ActiveRecord::RecordNotFound
-      nil
-    end
-
-    def find_remote(username, domain)
-      find_remote!(username, domain)
-    rescue ActiveRecord::RecordNotFound
-      nil
-    end
-
     def triadic_closures(account, limit: 5, offset: 0, exclude_ids: [])
-=======
-    def triadic_closures(account, limit: 5, offset: 0)
->>>>>>> 947887f261f74f84312327a5265553e8f16655fe
       sql = <<-SQL.squish
         WITH first_degree AS (
           SELECT target_account_id
