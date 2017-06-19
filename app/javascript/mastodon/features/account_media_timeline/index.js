6717d73571b7e3383953810c58feb24393c79acd
alpaca-tc <alpaca-tc@alpaca.tc>
2017-06-19 16:58:58 +0900

    Fixes AccountMediaTimeline

--- app/javascript/mastodon/features/account_media_timeline/index.js
+++ app/javascript/mastodon/features/account_media_timeline/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import {
-  fetchAccount,
-  fetchAccountMediaTimeline,
-  expandAccountMediaTimeline,
-} from '../../actions/accounts';
+import { fetchAccount } from '../../actions/accounts';
+import { refreshAccountMediaTimeline, expandAccountMediaTimeline } from '../../actions/timelines';
 import StatusList from '../../components/status_list';
 import LoadingIndicator from '../../components/loading_indicator';
 import Column from '../ui/components/column';
@@ -15,9 +12,9 @@ import ColumnBackButton from '../../components/column_back_button';
 import Immutable from 'immutable';
 
 const mapStateToProps = (state, props) => ({
-  statusIds: state.getIn(['timelines', 'accounts_media_timelines', Number(props.params.accountId), 'items'], Immutable.List()),
-  isLoading: state.getIn(['timelines', 'accounts_media_timelines', Number(props.params.accountId), 'isLoading']),
-  hasMore: !!state.getIn(['timelines', 'accounts_media_timelines', Number(props.params.accountId), 'next']),
+  statusIds: state.getIn(['timelines', `account:${props.params.accountId}:media`, 'items'], Immutable.List()),
+  isLoading: state.getIn(['timelines', `account:${props.params.accountId}:media`, 'isLoading'], false),
+  hasMore: !!state.getIn(['timelines', `account:${props.params.accountId}:media`, 'next'], false),
   me: state.getIn(['meta', 'me']),
 });
 
@@ -34,18 +31,20 @@ class AccountMediaTimeline extends React.PureComponent {
 
   componentWillMount () {
     this.props.dispatch(fetchAccount(Number(this.props.params.accountId)));
-    this.props.dispatch(fetchAccountMediaTimeline(Number(this.props.params.accountId)));
+    this.props.dispatch(refreshAccountMediaTimeline(Number(this.props.params.accountId)));
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.params.accountId !== this.props.params.accountId && nextProps.params.accountId) {
       this.props.dispatch(fetchAccount(Number(nextProps.params.accountId)));
-      this.props.dispatch(fetchAccountMediaTimeline(Number(nextProps.params.accountId)));
+      this.props.dispatch(refreshAccountMediaTimeline(Number(this.props.params.accountId)));
     }
   }
 
   handleScrollToBottom = () => {
-    this.props.dispatch(expandAccountMediaTimeline(Number(this.props.params.accountId)));
+    if (this.props.hasMore) {
+      this.props.dispatch(expandAccountMediaTimeline(Number(this.props.params.accountId)));
+    }
   }
 
   render () {
