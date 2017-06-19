c53ee409093af5e1ee1c65fae11e2e7320ae91e0
orekyuu <orekyuu@pixiv.com>
2017-06-19 10:13:50 +0900

    visibilityのチェックを追加

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -33,7 +33,7 @@ class PostStatusService < BaseService
       attach_pixiv_cards(status)
     end
 
-    if status&.media_attachments&.any? { |m| m.music_info.present? }
+    if status&.media_attachments&.any? { |m| m.music_info.present? } && status&.visibility == :public
       begin
         link = "https://#{Rails.configuration.x.local_domain}/web/statuses/#{status.id}"
         Playlist.new(Playlist::MEDIA_TL_DECK_ID).add(link, account, true)
