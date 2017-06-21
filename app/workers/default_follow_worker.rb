cbba806d46dc5897ea1ff83b8962271c50618264
ABCanG <abcang1015@gmail.com>
2017-06-21 15:16:02 +0900

    find_by -> find_local

--- app/workers/default_follow_worker.rb
+++ app/workers/default_follow_worker.rb
@@ -5,7 +5,7 @@ class DefaultFollowWorker
 
   def perform(account_id)
     account = Account.find(account_id)
-    default_follow_account = Account.find_by!(username: 'now_playing')
+    default_follow_account = Account.find_local!('now_playing')
     FollowService.new.call(account, default_follow_account.acct)
   rescue ActiveRecord::RecordNotFound
     nil
