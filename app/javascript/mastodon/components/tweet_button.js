af29ee4f54ab511c2e792672b22e88dcb00e94e9
ABCanG <abcang1015@gmail.com>
2017-06-22 17:52:38 +0900

    add noreferrer

--- app/javascript/mastodon/components/tweet_button.js
+++ app/javascript/mastodon/components/tweet_button.js
@@ -17,7 +17,7 @@ export default class TweetButton extends React.PureComponent {
     const url = encodeURIComponent(this.props.url);
 
     return (
-      <a target='_blank' rel='noopener' href={`https://twitter.com/intent/tweet?text=${text}&url=${url}`} className='twitter-share-button'>
+      <a target='_blank' rel='noopener noreferrer' href={`https://twitter.com/intent/tweet?text=${text}&url=${url}`} className='twitter-share-button'>
         <div className='twitter-share-button__wrapper'>
           <img src='/player/logos/twitter.png' width='14' height='14' className='twitter-share-button__image' />
           <span className='twitter-share-button__label'>ツイート</span>
