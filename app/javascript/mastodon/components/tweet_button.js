5a1d1234d52fd3149d76581c63f01d260be548ba
ABCanG <abcang1015@gmail.com>
2017-06-26 19:28:32 +0900

    improve tweet button

--- app/javascript/mastodon/components/tweet_button.js
+++ app/javascript/mastodon/components/tweet_button.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import queryString from 'query-string';
 
 export default class TweetButton extends React.PureComponent {
 
   static propTypes = {
     text: PropTypes.string.isRequired,
     url: PropTypes.string,
+    hashtags: PropTypes.string,
   };
 
   static defaultProps = {
     url: location.href,
+    hashtags: '',
   }
 
   render() {
-    const text = encodeURIComponent(this.props.text);
-    const url = encodeURIComponent(this.props.url);
+    const { text, url, hashtags } = this.props;
+    const params = { text, url };
+    if (hashtags) {
+      params.hashtags = hashtags;
+    }
 
     return (
-      <a target='_blank' rel='noopener noreferrer' href={`https://twitter.com/intent/tweet?text=${text}&url=${url}`} className='twitter-share-button'>
+      <a target='_blank' rel='noopener noreferrer' href={`https://twitter.com/intent/tweet?${queryString.stringify(params)}`} className='twitter-share-button'>
         <div className='twitter-share-button__wrapper'>
           <img src='/player/logos/twitter.png' width='14' height='14' className='twitter-share-button__image' />
           <span className='twitter-share-button__label'>ツイート</span>
