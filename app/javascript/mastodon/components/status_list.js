bca355572bc1f1476cacae811331e77a6fde0598
ABCanG <abcang1015@gmail.com>
2017-07-14 15:35:55 +0900

    Resolve a merged conflict - app/javascript/mastodon/components/status_list.js

--- app/javascript/mastodon/components/status_list.js
+++ app/javascript/mastodon/components/status_list.js
@@ -107,11 +107,7 @@ export default class StatusList extends ImmutablePureComponent {
   }
 
   render () {
-<<<<<<< HEAD
-    const { statusIds, onScrollToBottom, scrollKey, trackScroll, shouldUpdateScroll, isLoading, hasMore, prepend, emptyMessage, squareMedia, expandMedia, standalone, displayPinned } = this.props;
-=======
-    const { statusIds, scrollKey, trackScroll, shouldUpdateScroll, isLoading, hasMore, prepend, emptyMessage } = this.props;
->>>>>>> v1.4.7
+    const { statusIds, scrollKey, trackScroll, shouldUpdateScroll, isLoading, hasMore, prepend, emptyMessage, squareMedia, expandMedia, standalone, displayPinned } = this.props;
 
     let loadMore       = null;
     let scrollableArea = null;
