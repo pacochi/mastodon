952a64a85ac03bc559a56c0b51c7255ec35fb44e
ABCanG <abcang1015@gmail.com>
2017-07-14 16:16:45 +0900

    Resolve a merged conflict - app/javascript/mastodon/features/ui/components/report_modal.js

--- app/javascript/mastodon/features/ui/components/report_modal.js
+++ app/javascript/mastodon/features/ui/components/report_modal.js
@@ -8,13 +8,9 @@ import { makeGetAccount } from '../../../selectors';
 import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
 import StatusCheckBox from '../../report/containers/status_check_box_container';
 import Immutable from 'immutable';
-<<<<<<< HEAD:app/javascript/mastodon/features/report/index.js
 import Toggle from 'react-toggle';
-import ColumnBackButtonSlim from '../../components/column_back_button_slim';
-=======
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import Button from '../../../components/button';
->>>>>>> v1.4.7:app/javascript/mastodon/features/ui/components/report_modal.js
 
 const messages = defineMessages({
   placeholder: { id: 'report.placeholder', defaultMessage: 'Additional comments' },
@@ -57,29 +53,25 @@ export default class ReportModal extends ImmutablePureComponent {
     intl: PropTypes.object.isRequired,
   };
 
-<<<<<<< HEAD:app/javascript/mastodon/features/report/index.js
   state = { option: false };
+  options = Immutable.fromJS([
+    { id: 'donotlike', value: '好みではない' },
+    { id: 'incorrectage', value: 'CW・NSFW設定の不足' },
+    { id: 'spam', value: 'スパム・迷惑行為' },
+    { id: 'reproduction', value: '無断転載' },
+    { id: 'prohibited', value: '禁止行為に該当' },
+  ]);
 
-  componentWillMount () {
-    if (!this.props.account) {
-      this.context.router.history.replace('/');
-    }
-
-    this.options = Immutable.fromJS([
-      { id: 'donotlike', value: '好みではない' },
-      { id: 'incorrectage', value: 'CW・NSFW設定の不足' },
-      { id: 'spam', value: 'スパム・迷惑行為' },
-      { id: 'reproduction', value: '無断転載' },
-      { id: 'prohibited', value: '禁止行為に該当' },
-    ]);
-=======
   handleCommentChange = (e) => {
     this.props.dispatch(changeReportComment(e.target.value));
->>>>>>> v1.4.7:app/javascript/mastodon/features/ui/components/report_modal.js
   }
 
   handleSubmit = () => {
     this.props.dispatch(submitReport());
+  }
+
+  handlePreSubmit = () => {
+    this.setState({ option: true });
   }
 
   componentDidMount () {
@@ -92,27 +84,11 @@ export default class ReportModal extends ImmutablePureComponent {
     }
   }
 
-<<<<<<< HEAD:app/javascript/mastodon/features/report/index.js
-  handleCommentChange = (e) => {
-    this.props.dispatch(changeReportComment(e.target.value));
-  }
-
-  handleSubmit = () => {
-    this.props.dispatch(submitReport());
-    this.context.router.history.replace('/');
-  }
-
-  handlePreSubmit = () => {
-    this.setState({ option: true });
-  }
-
   onToggle = (e) => {
     this.props.dispatch(changeReportComment('')); // FIXME: Re render self
     this.props.dispatch(changeReportComment(e.target.getAttribute('name')));
   }
 
-=======
->>>>>>> v1.4.7:app/javascript/mastodon/features/ui/components/report_modal.js
   render () {
     const { account, comment, intl, statusIds, isSubmitting } = this.props;
     const filled = this.options.findIndex(option => option.get('value') === comment) > -1;
@@ -134,8 +110,7 @@ export default class ReportModal extends ImmutablePureComponent {
             </div>
           </div>
 
-<<<<<<< HEAD:app/javascript/mastodon/features/report/index.js
-          <div className='report__textarea-wrapper' style={{ padding: '10px' }}>
+          <div className='report-modal__comment'>
             {this.state.option &&
               <div className='report__select'>
                 <div>
@@ -151,7 +126,7 @@ export default class ReportModal extends ImmutablePureComponent {
                     </div>
                   )}
                   <textarea
-                    className={`report__textarea ${filled ? 'filled' : ''}`}
+                    className={`setting-text light ${filled ? 'filled' : ''}`}
                     placeholder={intl.formatMessage(messages.placeholder)}
                     value={filled ? '' : comment}
                     onChange={this.handleCommentChange}
@@ -162,24 +137,11 @@ export default class ReportModal extends ImmutablePureComponent {
                 </div>
               </div>
             }
-            <div className='report__submit'>
-              <Button disabled={isSubmitting} text={intl.formatMessage(messages.submit)} onClick={this.handlePreSubmit} block />
-            </div>
-=======
-          <div className='report-modal__comment'>
-            <textarea
-              className='setting-text light'
-              placeholder={intl.formatMessage(messages.placeholder)}
-              value={comment}
-              onChange={this.handleCommentChange}
-              disabled={isSubmitting}
-            />
->>>>>>> v1.4.7:app/javascript/mastodon/features/ui/components/report_modal.js
           </div>
         </div>
 
         <div className='report-modal__action-bar'>
-          <Button disabled={isSubmitting} text={intl.formatMessage(messages.submit)} onClick={this.handleSubmit} />
+          <Button disabled={isSubmitting} text={intl.formatMessage(messages.submit)} onClick={this.handlePreSubmit} />
         </div>
       </div>
     );
