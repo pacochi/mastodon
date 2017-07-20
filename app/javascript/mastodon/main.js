4159d1d0e76acad6da23440dc7dcaf9026e62904
ABCanG <abcang1015@gmail.com>
2017-07-20 20:30:43 +0900

    Fix eslint react/jsx-boolean-value: 2

--- app/javascript/mastodon/main.js
+++ app/javascript/mastodon/main.js
@@ -39,7 +39,7 @@ function main() {
 
     ReactDOM.render(<Mastodon {...props} />, mountNode);
     if (mountAboutPlayControl) {
-      ReactDOM.render(<PlayControl isTop={true} onError={function(){}} onSkip={function(){}} streamingAPIBaseURL={playControlInitialState.streaming_api_base_url} accessToken={playControlInitialState.access_token} />, mountAboutPlayControl);
+      ReactDOM.render(<PlayControl isTop onError={function(){}} onSkip={function(){}} streamingAPIBaseURL={playControlInitialState.streaming_api_base_url} accessToken={playControlInitialState.access_token} />, mountAboutPlayControl);
     }
     perf.stop('main()');
   });
