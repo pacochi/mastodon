6d8da86231726844f45ae846908e1bf87487a79c
ABCanG <abcang1015@gmail.com>
2017-08-03 14:45:45 +0900

    Comiket tags for web

--- app/javascript/mastodon/features/compose/containers/announcements_container.js
+++ app/javascript/mastodon/features/compose/containers/announcements_container.js
@@ -1,10 +1,20 @@
 import { connect }   from 'react-redux';
 import Announcements from '../components/announcements';
+import { addColumn } from '../../../actions/columns';
 
 const mapStateToProps = (state) => {
   return {
     account: state.getIn(['accounts', state.getIn(['meta', 'me'])]),
+    pinnedComiketTag: state.getIn(['settings', 'columns']).find((colmun) => {
+      return colmun.get('id') === 'SUGGESTION_TAGS' && colmun.getIn(['params', 'type']) === 'comiket';
+    }),
   };
 };
 
-export default connect(mapStateToProps)(Announcements);
+const mapDispatchToProps = (dispatch) => ({
+  addComiketTagColumn () {
+    dispatch(addColumn('SUGGESTION_TAGS', { type: 'comiket' }));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Announcements);
