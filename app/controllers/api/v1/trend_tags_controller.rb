6478b1709335f7edb04a69a56daba8e28dcc9c33
ABCanG <abcang1015@gmail.com>
2017-08-15 11:05:21 +0900

    Remove comiket tag from nav and announcement

--- app/controllers/api/v1/trend_tags_controller.rb
+++ app/controllers/api/v1/trend_tags_controller.rb
@@ -4,17 +4,9 @@ class Api::V1::TrendTagsController < Api::BaseController
   respond_to :json
 
   def index
-    # iOSアプリではおすすめタグの場所にコミケタグを表示する
-    is_ios_app = request.env['HTTP_USER_AGENT'].start_with?('PawooiOSApp/')
+    limit_size = params[:limit] || 5
+    limit = limit_size.to_i.clamp(0, 10)
 
-    if is_ios_app
-      limit = 30
-      suggestion_type = :comiket
-    else
-      limit_size = params[:limit] || 5
-      limit = limit_size.to_i.clamp(0, 10)
-      suggestion_type = :normal
-    end
-    @trend_tags = TrendTag.find_tags(limit, suggestion_type)
+    @trend_tags = TrendTag.find_tags(limit)
   end
 end
--- app/javascript/mastodon/features/compose/containers/announcements_container.js
+++ app/javascript/mastodon/features/compose/containers/announcements_container.js
@@ -1,20 +1,10 @@
 import { connect }   from 'react-redux';
 import Announcements from '../components/announcements';
-import { addColumn } from '../../../actions/columns';
 
 const mapStateToProps = (state) => {
   return {
     account: state.getIn(['accounts', state.getIn(['meta', 'me'])]),
-    pinnedComiketTag: state.getIn(['settings', 'columns']).find((colmun) => {
-      return colmun.get('id') === 'SUGGESTION_TAGS' && colmun.getIn(['params', 'type']) === 'comiket';
-    }),
   };
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  addComiketTagColumn () {
-    dispatch(addColumn('SUGGESTION_TAGS', { type: 'comiket' }));
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Announcements);
+export default connect(mapStateToProps)(Announcements);
