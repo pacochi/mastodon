d39acd1e372899b0b125df1a441807baf1ad8593
Gagne <ganie@pe.ska.life.tsukuba.ac.jp>
2017-09-05 17:16:05 +0900

    おすすめユーザー取得のためのSQLクエリの改善
    3日以内にトゥートのあったアカウントのみを取得するようにしました。

--- app/models/account.rb
+++ app/models/account.rb
@@ -165,7 +165,7 @@ class Account < ApplicationRecord
       reorder(nil).pluck('distinct accounts.domain')
     end
 
-    def triadic_closures(account, limit: 5, offset: 0, exclude_ids: [])
+    def triadic_closures(account, limit: 5, offset: 0, exclude_ids: [], current_time: Time.current)
       sql = <<-SQL.squish
         WITH first_degree AS (
           SELECT target_account_id
@@ -183,6 +183,12 @@ class Account < ApplicationRecord
           AND target_account_id NOT IN (:excluded_account_ids)
           AND accounts.suspended = false
         GROUP BY target_account_id, accounts.id
+        HAVING (
+          SELECT created_at
+          FROM statuses
+          WHERE statuses.account_id = target_account_id
+          ORDER BY statuses.id DESC LIMIT 1
+        ) >= TIMESTAMP :current_timestamp - '3 days'::INTERVAL
         ORDER BY count(account_id) DESC
         OFFSET :offset
         LIMIT :limit
@@ -191,7 +197,7 @@ class Account < ApplicationRecord
       excluded_account_ids = account.excluded_from_timeline_account_ids + [account.id] + exclude_ids
 
       find_by_sql(
-        [sql, { account_id: account.id, excluded_account_ids: excluded_account_ids, limit: limit, offset: offset }]
+        [sql, { account_id: account.id, excluded_account_ids: excluded_account_ids, limit: limit, offset: offset, current_timestamp: current_time }]
       )
     end
 
