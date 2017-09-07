0de39700fb115e60471f1c9659c08be3d3d357f2
ABCanG <abcang1015@gmail.com>
2017-09-07 12:55:58 +0900

    add pawoo_support

--- app/workers/default_follow_worker.rb
+++ app/workers/default_follow_worker.rb
@@ -3,11 +3,12 @@
 class DefaultFollowWorker
   include Sidekiq::Worker
 
+  DEFAULT_FOLLOW_USERS = %w(pixiv pawoo_support).freeze
+
   def perform(account_id)
     account = Account.find(account_id)
-    default_follow_account = Account.find_local!('pixiv')
-    FollowService.new.call(account, default_follow_account.acct)
-  rescue ActiveRecord::RecordNotFound
-    nil
+    Account.where(username: DEFAULT_FOLLOW_USERS, domain: nil).each do |default_follow_account|
+      FollowService.new.call(account, default_follow_account.acct)
+    end
   end
 end
