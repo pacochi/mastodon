5745456933bbf000877bf4a4cc48548ba7b609b4
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-09-11 20:45:04 +0900

    Enforce unique option on VideoPreparingWorker

--- app/workers/video_preparing_worker.rb
+++ app/workers/video_preparing_worker.rb
@@ -3,6 +3,8 @@
 class VideoPreparingWorker
   include Sidekiq::Worker
 
+  sidekiq_options unique_for: 16.minutes
+
   def perform(id)
     music_attachment = MusicAttachment.joins(:status).includes(status: :account).find(id)
     NotifyService.new.call(music_attachment.status.account, music_attachment)
