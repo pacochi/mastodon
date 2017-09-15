96b597a8e2d027c5202410d9219b734e6a0a8527
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-09-15 21:30:16 +0900

    Add music seed to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,38 @@ if Rails.env.development?
   admin  = Account.where(username: 'admin').first_or_initialize(username: 'admin')
   admin.save(validate: false)
   User.where(email: "admin@#{domain}").first_or_initialize(email: "admin@#{domain}", password: 'mastodonadmin', password_confirmation: 'mastodonadmin', confirmed_at: Time.now.utc, admin: true, account: admin).save!
+
+  # Additional seeds of Pawoo Music
+  account = User.find_by!(email: "admin@#{domain}").account
+
+  music_attachment_status = Status.new(account: account, text: '', visibility: :unlisted)
+  music_attachment_status.save! validate: false
+
+  music_attachment = MusicAttachment.create!(
+    music: File.open(Rails.root.join('spec', 'fixtures', 'files', 'aint_we_got_fun_billy_jones1921.mp3')),
+    image: File.open(Rails.root.join('spec', 'fixtures', 'files', 'attachment.jpg')),
+    duration: 1.minute,
+    title: "Ain't We Got Fun",
+    artist: 'Billy Jones',
+    status: music_attachment_status
+  )
+
+  music_attachment_status.update! text: Rails.application.routes.url_helpers.short_account_track_url(account.username, music_attachment)
+
+  album_status = Status.new(account: account, text: '', visibility: :unlisted)
+  album_status.save! validate: false
+
+  album = Album.create!(
+    image: File.open(Rails.root.join('spec', 'fixtures', 'files', 'attachment.jpg')),
+    title: 'Digital History',
+    status: album_status
+  )
+
+  album_status.update! text: Rails.application.routes.url_helpers.short_account_album_url(account.username, album)
+
+  AlbumMusicAttachment.create!(
+    album: album,
+    music_attachment: music_attachment,
+    position: '0.5'
+  )
 end
