5127d8cc20ed7ab358a1b3ab4e4fd583f73a69c8
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-09-15 18:57:13 +0900

    Update specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe 'GET #index' do
-    it 'renders default' do
+    it 'renders' do
       sign_in(Fabricate(:user))
       get :index
 
-      expect(assigns(:appmode)).to eq 'default'
       expect(response).to have_http_status(:success)
     end
   end
