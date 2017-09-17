6cb30246afd46cc4f752a049b941cdb15563d49e
ABCanG <abcang1015@gmail.com>
2017-09-17 18:10:26 +0900

    Fix cache of status_pin

--- app/controllers/api/v1/statuses/pins_controller.rb
+++ app/controllers/api/v1/statuses/pins_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::Statuses::PinsController < Api::BaseController
   before_action -> { doorkeeper_authorize! :write }
   before_action :require_user!
   before_action :set_status
+  after_action :clear_cache
 
   respond_to :json
 
@@ -24,5 +25,10 @@ class Api::V1::Statuses::PinsController < Api::BaseController
 
   def set_status
     @status = Status.find(params[:status_id])
+  end
+
+  def clear_cache
+    # キャッシュにstatus_pinsの情報も保存されているためクリアする
+    Rails.cache.delete(@status.cache_key)
   end
 end
