fd81218a4089d164ba2dd0db6992b7e154b29ff5
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-10-05 17:15:34 +0900

    Update webpack babel configuration

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -3,7 +3,7 @@ module.exports = {
   // include react-intl because transform-react-remove-prop-types needs to apply to it
   exclude: {
     test: /node_modules/,
-    exclude: /react-intl[\/\\](?!locale-data)/,
+    exclude: /(react-intl[\/\\](?!locale-data))|(musicvideo-generator)/,
   },
   loader: 'babel-loader',
   options: {
