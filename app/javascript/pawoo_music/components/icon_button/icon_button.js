9ce05edf5df45e37f3203c09c317169ee53a8e30
geta6 <getakura@gmail.com>
2017-10-12 10:50:09 +0900

    Add unclickable mode to IconButton component

--- app/javascript/pawoo_music/components/icon_button/icon_button.js
+++ app/javascript/pawoo_music/components/icon_button/icon_button.js
@@ -1,40 +1,44 @@
+import noop from 'lodash/noop';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import feather from 'feather-icons';
 
-export default class IconButton extends PureComponent {
+export default class Icon extends PureComponent {
 
   static propTypes = {
     src: PropTypes.string.isRequired,
     onClick: PropTypes.func,
     title: PropTypes.string,
+    tabindex: PropTypes.number,
     disabled: PropTypes.bool,
     className: PropTypes.string,
   }
 
   static defaultProps = {
-    title: 'icon button',
+    title: 'icon',
+    tabindex: -1,
   };
 
-  handleClick = (e) => {
+  isClickable = () => {
     const { onClick, disabled } = this.props;
-    if (!disabled && onClick) {
-      onClick(e);
-    }
+    return !disabled && !!onClick;
   }
 
   render () {
-    const { src, title, className } = this.props;
+    const { src, title, tabindex, className, onClick } = this.props;
+    const svg = feather.toSvg(src, { 'stroke-width': 1 });
+    const clickable = this.isClickable();
 
     return (
       <span
-        dangerouslySetInnerHTML={{
-          __html: feather.toSvg(src, {
-            'stroke-width': 1,
-          }),
-        }}
-      className={classNames('icon-button', className)} title={title} role='button' tabIndex='0' aria-pressed='false' onClick={this.handleClick}
+        dangerouslySetInnerHTML={{ __html: svg }}
+        className={classNames('icon-button', { clickable }, className)}
+        onClick={clickable ? onClick : noop}
+        title={title}
+        role={clickable ? 'button' : 'presentation'}
+        tabIndex={tabindex}
+        ariaPressed='false'
       />
     );
   }
