56ae4a0445ac27ad6c16987d1444c06489b03822
Norio Shimizu <toykyo.net@gmail.com>
2017-10-13 01:56:36 +0900

    fix error

--- app/javascript/pawoo_music/containers/community_timeline/community_timeline.js
+++ app/javascript/pawoo_music/containers/community_timeline/community_timeline.js
@@ -7,7 +7,7 @@ import StatusTimelineContainer from '../../containers/status_timeline';
 import { connectCommunityStream } from '../../actions/streaming';
 import { updateTimelineTitle } from '../../actions/timeline';
 import { changeFooterType } from '../../actions/footer';
-import MediaPostButton from '../../components/media_post_button';
+import MediaPost from '../../components/media_post';
 
 @connect()
 export default class CommunityTimelineContainer extends PureComponent {
@@ -42,7 +42,7 @@ export default class CommunityTimelineContainer extends PureComponent {
         timelineId='community'
         loadMore={this.handleLoadMore}
         emptyMessage={<FormattedMessage id='empty_column.community' defaultMessage='The local timeline is empty. Write something publicly to get the ball rolling!' />}
-        galleryPrepend={<MediaPostButton />}
+        galleryPrepend={<MediaPost />}
         withComposeForm
       />
     );
