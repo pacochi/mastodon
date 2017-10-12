62b6b2125f60efe8078fcd920f43f5ee864a511c
geta6 <getakura@gmail.com>
2017-10-12 09:53:26 +0900

    Modalize track media post

--- app/javascript/pawoo_music/components/media_post_button/media_post_button.js
+++ app/javascript/pawoo_music/components/media_post_button/media_post_button.js
@@ -1,13 +1,27 @@
+import UAParser from 'ua-parser-js';
 import React, { PureComponent } from 'react';
 import IconButton from '../icon_button';
 
 export default class MediaPostButton extends PureComponent {
 
+  constructor(props, context) {
+    super(props, context);
+    this.mobile = ['mobile', 'tablet'].includes(new UAParser().getDevice().type);
+  }
+
+  handleMediaPost = () => {
+    if (this.mobile) {
+      location.href = '/tracks/new';
+    } else {
+      location.href = '/tracks/new';
+    }
+  };
+
   render () {
     return (
-      <a className='media-post-button' href='/tracks/new'>
+      <div className='media-post-button' onClick={this.handleMediaPost}>
         <IconButton src='plus' />
-      </a>
+      </div>
     );
   }
 
