26ff0844f6af5d69acc3b5efc2a9cf7e902864f0
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-10-19 18:26:28 +0900

    Fix slow query

--- app/models/concerns/status_pawoo_music_concern.rb
+++ app/models/concerns/status_pawoo_music_concern.rb
@@ -6,7 +6,7 @@ module StatusPawooMusicConcern
   included do
     after_destroy { self.music&.destroy! unless self.reblog? }
     belongs_to :music, polymorphic: true
-    scope :musics_only, -> { where(music_type: ['Track', 'Album']) }
+    scope :musics_only, -> { where(music_type: 'Track') } # TODO: include Album
     scope :tracks_only, -> { where(music_type: 'Track') }
     scope :albums_only, -> { where(music_type: 'Album') }
     counter_culture :account, column_name: -> (model) { (!model.reblog? && !model.music_type.nil?) ? "#{model.music_type.downcase.pluralize}_count" : nil }
