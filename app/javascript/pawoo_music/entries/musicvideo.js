cc71371987bbd305ead68105387cd3e8d51f0175
ABCanG <abcang1015@gmail.com>
2017-10-19 14:00:12 +0900

    Fix music oembed

--- app/javascript/pawoo_music/entries/musicvideo.js
+++ app/javascript/pawoo_music/entries/musicvideo.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 import { Provider } from 'react-redux';
 import { IntlProvider, addLocaleData } from 'react-intl';
-import classNames from 'classnames';
 import configureStore from '../../mastodon/store/configureStore';
 import { getLocale } from '../../mastodon/locales';
 import Track from '../containers/track';
@@ -29,7 +28,7 @@ export default class MusicvideoEntry extends React.PureComponent {
     return (
       <IntlProvider locale={locale} messages={messages}>
         <Provider store={store}>
-          <div className={classNames('app', 'sp')} style={{ width: 480 }}>
+          <div className='app' style={{ width: 'var(--width-gallery)' }}>
             <Track track={Immutable.fromJS(track)} />
           </div>
         </Provider>
